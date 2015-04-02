74b4d625deef061bc84ae226b0b4478c04b609f4
Jeremiah Kelly <emkayoh@mac.com>
2015-04-02 08:48:48 +0100

    model change
    the model in now the continuos one

--- TestData.R
+++ TestData.R
@@ -35,9 +35,6 @@ TestData.default <- function(x, theta = NULL, sse = 0.03) {
 	Noise <- rnorm(Len, 0, sqrt(sse))
 	Y <- theta[1]+theta[2]*exp(-x*theta[3])+ theta[4]*(x-theta[5])*H(x,10,theta[5]) + theta[6]*(x-theta[7])*H(x,10,theta[7])
 	
-	# theta[1] + theta[2] * exp(-x * theta[3]) + theta[4] * pmax(0, x - theta[5]) + theta[6] * 
-		# pmax(0, x - theta[7])
-
 	tmp <- list(call = match.call(), time = x, thrs = (Y + Noise), resid = Noise, fit = Y, 
 		thet = theta, sse = sse, val = var(Noise), data=paste('Test_data_', sse, sep=''))
 	class(tmp) = "dark"
