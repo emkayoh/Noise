e177f50124e5107b21897477a78c09b31e4c4252
Jeremiah Kelly <emkayoh@mac.com>
2015-04-02 12:08:11 +0100

    uses lIst to sample test data

--- Experiment.R
+++ Experiment.R
@@ -4,7 +4,7 @@
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
 
-    # http://www.apache.org/licenses/LICENSE-2.0
+# http://www.apache.org/licenses/LICENSE-2.0
 
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
@@ -12,51 +12,46 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
- rm(list=ls())
- 
+rm(list = ls())
+
 # Numerical experiment to evaluate the effect of signal noise 
 # on model selection and parameter estimates
-source('~/GitHub/Noise/AICc.R')
-source('~/GitHub/Noise/BestFit.R')
-source('~/GitHub/Noise/BootDark.R')
-source('~/GitHub/Noise/Declutter.R')
-source('~/GitHub/Noise/Models2.R')
-source('~/GitHub/Noise/ModelSelect.R')
-source('~/GitHub/Noise/MultiStart.R')
-source('~/GitHub/Noise/Start.R')
-source('~/GitHub/Noise/TestData.R')
+source("~/GitHub/Noise/AICc.R")
+source("~/GitHub/Noise/BestFit.R")
+source("~/GitHub/Noise/BootDark.R")
+source("~/GitHub/Noise/Declutter.R")
+source("~/GitHub/Noise/GetTestData.R")
+source("~/GitHub/Noise/Models2.R")
+source("~/GitHub/Noise/ModelSelect.R")
+source("~/GitHub/Noise/MultiStart.R")
+source("~/GitHub/Noise/Start.R")
+source("~/GitHub/Noise/TestData.R")
 
-Draw=F
-List <- list()
-Pts<-1:40
- 
-Repeats <- 200
-x<- seq(0,20, by=0.5)
+Draw = T
+load(GetTestData(-0.24, 2)) # loads the 1000 test data sets in a list called List
+Pts <- 1
+Repeats <- 1000
 
-sse=0.01
+for (ii in Pts) {
+	tmp <- List[[ii]]
 
-for(ii in Pts){
-theta<-c(-1.5, 1, 1, -0.24, 6, 0.06, 15)
-tmp<- TestData(x, sse=sse, theta=theta)
-tmp1<- Declutter(tmp)
+	x <- tmp$time
+	y <- tmp$thrs
 
 
-x<-tmp1$time
-y<-tmp1$thrs
-if(Draw){
-par(mfrow=c(2,2), las=1, bty='n')
-plot(tmp$time, tmp$thrs)
-points(tmp1$time, tmp1$thrs, pch=16, col=2)
+	if (Draw) {
+		par(mfrow = c(2, 2), las = 1, bty = "n")
+		plot(tmp$time, tmp$thrs)
+	}
+
+	P <- Start(tmp, Repeats)
+	MSC <- ModelSelect(tmp, P)
+	tmp1 <- BestFit(tmp, MSC, draw = Draw)
+
+	tmp1 <- MultiStart_2(tmp1, repeats = as.integer(Repeats/5), draw = Draw)
+	Out <- BootDark(tmp1, R = 2 * Repeats, graph = Draw)
 }
 
-P<-Start(tmp1,Repeats)
-MSC<-ModelSelect(tmp1, P)
-tmp2<-BestFit(tmp1, MSC,draw = Draw)
-
-
-
-tmp3<-MultiStart_2(tmp2,repeats = as.integer(Repeats/5),draw=Draw)
-Out<-BootDark(tmp3,R=2*Repeats, graph = Draw)
- List[[length(List)+1]] <- list(Out)
+# List[[length(List)+1]] <- list(Out)
+# rm(tmp1, tmp)
 # save(List, file=File)
- }
